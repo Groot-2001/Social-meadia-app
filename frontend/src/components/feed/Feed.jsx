3e74de5b61d2353cd3fd431897d64296e7e2e151
Groot-2001 <shivasilmawala841@gmail.com>
2022-06-17 17:38:50 +0530

    proxy server added to frontend

--- frontend/src/components/feed/Feed.jsx
+++ frontend/src/components/feed/Feed.jsx
@@ -1,16 +1,29 @@
+import { useEffect } from "react";
+import { useState } from "react"
 import Post from "../post/Post"
 import Share from "../share/Share"
 import "./feed.css"
-import { Posts } from "../../dummyData";
+import axios from "axios";
 
 export default function Feed() {
+  const [posts,setPost] = useState([]);
+ 
+
+  useEffect(()=>{
+    const fetchPost = async () =>{
+      const res = axios.get("posts/timeline/629dbd87bac17b1c564c7cd8")
+      console.log(res);
+    }
+    fetchPost();
+  },[]);
+
   return (
     <div className="feed">
       <div className="feed-wrapper">
         <Share/>
-        {Posts.map((p)=>(
+        {/* {Posts.map((p)=>(
           <Post key={p.id} post={p}/>
-        ))}
+        ))} */}
         
       </div>
     </div>
--- frontend/src/dummyData.js
+++ frontend/src/dummyData.js
@@ -1,27 +1,27 @@
 export const user = [
     {
         id:1,
-        profilepicture:"/assets/person/icon.png",
+        profilepicture:"person/icon.png",
         username:"Shiva Silmawala"
     },
     {
         id:2,
-        profilepicture:"/assets/person/2.jpeg",
+        profilepicture:"person/2.jpeg",
         username:"Rocky Raja"
     },
     {
         id:3,
-        profilepicture:"/assets/person/3.jpeg",
+        profilepicture:"person/3.jpeg",
         username:"Rohit jhunjhunwala"
     },
     {
         id:4,
-        profilepicture:"/assets/person/4.jpeg",
+        profilepicture:"person/4.jpeg",
         username:"karan Teja"
     },
     {
         id:5,
-        profilepicture:"/assets/person/5.jpeg",
+        profilepicture:"person/5.jpeg",
         username:"pappu Ahuja"
     }
 ];
@@ -30,7 +30,7 @@ export const Posts =[
     {
         id:1,
         desc:"hey good morning to all",
-        photo:"/assets/post/1.jpeg",
+        photo:"post/1.jpeg",
         userId:1,
         like:32,
         comment:9
@@ -38,7 +38,7 @@ export const Posts =[
     {
         id:2,
         desc:"Love for all,hatred for none.",
-        photo:"/assets/post/2.jpeg",
+        photo:"post/2.jpeg",
         userId:2,
         like:25,
         comment:13
@@ -46,7 +46,7 @@ export const Posts =[
     {
         id:3,
         desc:"god is great ,the greatest god,i am greatful",
-        photo:"/assets/post/3.jpeg",
+        photo:"post/3.jpeg",
         userId:3,
         like:39,
         comment:15
@@ -54,7 +54,7 @@ export const Posts =[
     {
         id:4,
         desc:"good vibes only",
-        photo:"/assets/post/4.jpeg",
+        photo:"post/4.jpeg",
         userId:4,
         like:42,
         comment:11
@@ -62,7 +62,7 @@ export const Posts =[
     {
         id:5,
         desc:"Everyday is a new opportunity",
-        photo:"/assets/post/5.jpeg",
+        photo:"post/5.jpeg",
         userId:5,
         like:51,
         comment:23
