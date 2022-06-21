9541421bcd1d36a664b87288a0b7d14b9f5e77c9
Groot-2001 <shivasilmawala841@gmail.com>
2022-06-21 16:27:18 +0530

    added the login logic using context-api

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,30 @@
+import {createContext, useReducer} from "react";
+
+// initial state where default value is set
+const INITIAL_STATE = {
+    user:null,
+    isFetching:false,
+    error:false
+}
+
+//creating the context-api
+export const AuthContext = createContext(INITIAL_STATE);
+
+//wrapper where components are gonna use with context api
+
+export const AuthContextProvider = ({children})=> {
+    const [state,dispatch] = useReducer(AuthContext,INITIAL_STATE);
+
+    //returning the state with respect to children
+    return (
+        <AuthContext.Provider 
+            value={{
+                user:state.user,
+                isFetching:state.isFetching,
+                error:state.error,
+                dispatch
+                }}>
+            {children}    
+        </AuthContext.Provider>
+    ) 
+}
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,6 +1,13 @@
+import { useRef } from "react";
 import "./login.css";
 
 export default function Login() {
+    const email = useRef();
+    const password = useRef();
+    const handlfunc = (e) =>{
+        e.preventDefault();
+        console.log(email);
+    }
   return (
     <div className="login">
         <div className="login-wrapper">
@@ -10,15 +17,30 @@ export default function Login() {
                     Connect with friends and the world around you on FunChat.
                 </span>
             </div>
-            <div className="login-right">
+            <form className="login-right" onSubmit={handlfunc}>
                 <div className="login-box">
-                    <input placeholder="Email" className="login-input" />
-                    <input placeholder="Password" className="login-input" />
+                    <input 
+                        placeholder="Email" 
+                        type={"email"}
+                        required 
+                        className="login-input"
+                        ref={email}
+                    />
+
+                    <input 
+                        placeholder="Password" 
+                        type={"password"}
+                        required 
+                        minLength={6}
+                        className="login-input"
+                        ref={password}
+                    />
+
                     <button className="login-btn">Login</button>
                     <span className="password-forgot">forgot password ?</span>
                     <button className="create-button">Create An Account</button>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
   )
